8526ac5c74b14c51c8ca7a6fa7d617ab78b8cbd5
Binil Surendran <binil1931@gmail.com>
2015-05-20 11:15:46 +0530

    Initial commit

---
+++ app/src/main/java/com/polus/binil/navigationbarwithswiptabhost/FragmentB.java
@@ -0,0 +1,28 @@
+package com.polus.binil.navigationbarwithswiptabhost;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ */
+public class FragmentB extends SherlockFragment {
+
+    public FragmentB() {
+        // Required empty public constructor
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_b, container, false);
+    }
+
+
+}
---
+++ app/src/main/java/com/polus/binil/navigationbarwithswiptabhost/FragmentC.java
@@ -0,0 +1,30 @@
+package com.polus.binil.navigationbarwithswiptabhost;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+/**
+ * A simple {@link android.support.v4.app.Fragment} subclass.
+ */
+public class FragmentC extends SherlockFragment {
+
+
+    public FragmentC() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_c, container, false);
+    }
+
+
+}
